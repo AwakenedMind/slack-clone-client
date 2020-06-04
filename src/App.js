8b54f46ffb15061a43c39a1752e2b7341081c037
Robert Arteaga <daspanisharmada@gmail.com>
2020-06-03 20:48:38 -0700

    added apollo subscriptions for client

--- src/App.js
+++ src/App.js
@@ -1,12 +1,84 @@
 import React from 'react';
 import './App.css';
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from '@apollo/react-hooks';
 import Routes from './routes/';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { setContext } from 'apollo-link-context';
+import { ApolloLink, split } from 'apollo-link';
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
+
+const httpLink = createHttpLink({ uri: 'http://localhost:8000/graphql' });
+const middlewareLink = setContext(() => ({
+	headers: {
+		'x-token': localStorage.getItem('token'),
+		'x-refresh-token': localStorage.getItem('refreshToken'),
+	},
+}));
+
+const afterwareLink = new ApolloLink((operation, forward) => {
+	return forward(operation).map((response) => {
+		const {
+			response: { headers },
+		} = operation.getContext();
+
+		if (headers) {
+			const token = headers.get('x-token');
+			const refreshToken = headers.get('x-refresh-token');
+
+			if (token) {
+				localStorage.setItem('token', token);
+			}
+
+			if (refreshToken) {
+				localStorage.setItem('refreshToken', refreshToken);
+			}
+		}
+		return response;
+	});
+});
+
+const httpLinkWithMiddleware = afterwareLink.concat(
+	middlewareLink.concat(httpLink)
+);
+
+const wsLink = new WebSocketLink({
+	uri: 'ws://localhost:8000/subscriptions',
+	options: {
+		reconnect: true,
+		connectionParams: {
+			token: localStorage.getItem('token'),
+			refreshToken: localStorage.getItem('refreshToken'),
+		},
+	},
+});
+
+const link = split(
+	({ query }) => {
+		const { kind, operation } = getMainDefinition(query);
+		return kind === 'OperationDefinition' && operation === 'subscription';
+	},
+	wsLink,
+	httpLinkWithMiddleware
+);
 
 // Create the Apollo Client & connect to Apollo Server
 const client = new ApolloClient({
-	uri: 'http://localhost:8000/graphql',
+	link,
+	cache: new InMemoryCache(),
+	request: (operation) => {
+		const token = localStorage.getItem('token');
+		const refreshToken = localStorage.getItem('refreshToken');
+
+		operation.setContext({
+			headers: {
+				'x-token': token ? token : '',
+				'x-refresh-token': refreshToken ? refreshToken : '',
+			},
+		});
+	},
 });
 
 function App() {
